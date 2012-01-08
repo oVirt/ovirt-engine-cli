62e89042432c8f998d3379cf515ebf3e549deef4
Michael Pasternak <mpastern@redhat.com>
2012-01-08 12:35:34 +0200

    cli: add ASL2.0 license
    Change-Id: I45cd9750d18c7748440e8db47236ecf358323067

--- src/ovirtcli/platform/posix/util.py
+++ src/ovirtcli/platform/posix/util.py
@@ -1,6 +1,23 @@
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 
 import os.path
 import sys
+import pwd
 
 stdin_fileno = sys.stdin.fileno()
 stdout_fileno = sys.stdout.fileno()
@@ -58,6 +75,6 @@ def get_home_dir():
         return home
     try:
         pw = pwd.getpwuid(os.getuid())
-    except Keyerror:
+    except Exception:
         return None
     return pw.pw_dir
