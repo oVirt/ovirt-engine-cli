80680c9d3c5218cbd8d7becc8a686caed88fbb0b
Michael Pasternak <mpastern@redhat.com>
2012-02-08 13:28:59 +0200

    cli: refactor list command to support history stack
    Change-Id: I7319981c812fdf2e9024bf4f78fafcba9595a783

---
+++ src/ovirtcli/shell/listcmdshell.py
@@ -0,0 +1,43 @@
+#
+# Copyright (c) 2010 Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#           http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from ovirtcli.shell.cmdshell import CmdShell
+from ovirtcli.command.list import ListCommand
+
+
+class ListCmdShell(CmdShell):
+    NAME = 'list'
+
+    def __init__(self, context, parser):
+        CmdShell.__init__(self, context, parser)
+
+    def do_list(self, args):
+        return self.context.execute_string(ListCmdShell.NAME + ' ' + args + '\n')
+        
+    def help_list(self):
+        print ListCommand.helptext1
+
+    def complete_list(self, text, line, begidx, endidx):
+        ARGS=[]        
+        if not text:
+            completions = ARGS[:]
+        else:
+            completions = [ f
+                            for f in ARGS
+                            if f.startswith(text)
+                            ]
+        return completions
