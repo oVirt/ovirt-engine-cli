8fa2047ae5c634da67752b85c99a1f58a783e9e4
Michael Pasternak <mpastern@redhat.com>
2012-02-15 22:11:16 +0200

    cli: disable not supported options
    Change-Id: I601ab228fdb61547df18a3b884208eb2020cf50e

--- src/ovirtcli/options.py
+++ src/ovirtcli/options.py
@@ -30,12 +30,12 @@ class OvirtCliOptionParser(OptionParser):
 
         OptionParser.__init__(self, usage=self.usage,
                               description=self.description)
-        self.add_option('-d', '--debug', action='store_true',
-                        help='enable debugging')
-        self.add_option('-v', '--verbose', action='store_const',
-                        const=10, dest='verbosity', help='be more verbose')
-        self.add_option('-H', '--help-commands', action='store_true',
-                        help='show help on commands')
+#        self.add_option('-d', '--debug', action='store_true',
+#                        help='enable debugging')
+#        self.add_option('-v', '--verbose', action='store_const',
+#                        const=10, dest='verbosity', help='be more verbose')
+#        self.add_option('-H', '--help-commands', action='store_true',
+#                        help='show help on commands')
         self.add_option('-l', '--url',
                         help='specifies the API entry point URL')
         self.add_option('-u', '--username', help='connect as this user')
@@ -44,19 +44,19 @@ class OvirtCliOptionParser(OptionParser):
         self.add_option('-C', '--cert-file', help='specify cert-file')
         self.add_option('-P', '--port', help='specify port')
         self.add_option('-T', '--timeout', help='specify timeout')
-        self.add_option('-r', '--read-input', action='store_true',
-                        help='read pre-formatted input on stdin')
-        self.add_option('-i', '--input-format', metavar='FORMAT',
-                        help='input format for pre-formatted input')
-        self.add_option('-o', '--output-format', metavar='FORMAT',
-                         help='specfies the output format')
+#        self.add_option('-r', '--read-input', action='store_true',
+#                        help='read pre-formatted input on stdin')
+#        self.add_option('-i', '--input-format', metavar='FORMAT',
+#                        help='input format for pre-formatted input')
+#        self.add_option('-o', '--output-format', metavar='FORMAT',
+#                         help='specfies the output format')
         self.add_option('-c', '--connect', action='store_true',
                         help='automatically connect')
-        self.add_option('-f', '--filter', metavar='FILE',
-                        help='read commands from FILE instead of stdin')
-        self.add_option('-w', '--wide', action='store_true',
-                        help='wide display')
-        self.add_option('-n', '--no-header', action='store_false',
-                        dest='header', help='suppress output header')
-        self.add_option('-F', '--fields', help='fields to display')
+#        self.add_option('-f', '--filter', metavar='FILE',
+#                        help='read commands from FILE instead of stdin')
+#        self.add_option('-w', '--wide', action='store_true',
+#                        help='wide display')
+#        self.add_option('-n', '--no-header', action='store_false',
+#                        dest='header', help='suppress output header')
+#        self.add_option('-F', '--fields', help='fields to display')
         self.disable_interspersed_args()
