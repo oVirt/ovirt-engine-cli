654b476b5099bfee36df269866c1199a34da5e91
Michael Pasternak <mpastern@redhat.com>
2012-12-16 11:28:43 +0200

    cli: filter out _Base__context prop in txt formating
    _Base__context prop belongs to sdk internals
    
    Change-Id: Idc8765c4bcaa1f4787b65e90f8d866ae60564d56
    Signed-off-by: Michael Pasternak <mpastern@redhat.com>

--- src/ovirtcli/format/text.py
+++ src/ovirtcli/format/text.py
@@ -122,7 +122,6 @@ class TextFormatter(Formatter):
                                      else fil
 
         if mode != FormatMode.REDUCED or fil in reduced_mode_fields:
-#TODO: change to inline formating
             stdout.write(format0 % fil)
             stdout.write(': ')
             stdout.write(format1 % val)
@@ -138,7 +137,7 @@ class TextFormatter(Formatter):
         context = self.context
         settings = context.settings
         stdout = context.terminal.stdout
-        fields_exceptions = ['link', 'href', 'parentclass']
+        fields_exceptions = ['link', 'href', 'parentclass', '_Base__context']
         reduced_mode_fields = ['id', 'name', 'description']
 
         fields = self.sort_fields(self._get_fields(resource), sort_strategy)
@@ -222,74 +221,6 @@ class TextFormatter(Formatter):
                                       mode=FormatMode.REDUCED if not show_empty
                                                               else FormatMode.FULL)
             stdout.write('\n')
-
-##FIXME: add attribute/field overriding caps as in self._get_fields()
-##        info = schema.type_info(type(collection))
-##        fields = self._get_fields(info[0], 'L', scope)
-#
-##        collection_name = type(collection).__name__
-##        if collection_name.endswith('s'):
-##            candidate = collection_name[0 : len(collection_name) - 1]
-##        else:
-##            candidate = collection_name
-#        if len(collection) == 0: return
-#        fields = self._get_fields(params.findRootClass(ParseHelper.getXmlTypeInstance(type(collection[0].superclass).__name__.lower())),
-#                                                                                      'L',
-#                                                                                      scope)
-##        collection_member = ParseHelper.getSingularXmlTypeInstance(type(collection).__name__)
-#
-#        # Calculate the width of each column
-##        widths = [ len(f.name) for f in fields ]
-#
-#        if fields != None and len(fields) > 0:
-#            widths = [ len(f.name) for f in fields ]
-##        for resource in getattr(collection, collection_member):
-#            for resource in collection:
-#                for i in range(len(fields)):
-#                    value = fields[i].get(resource, self.context)
-#                    widths[i] = max(widths[i], len(value))
-#            total = sum(widths) + 2 * len(fields)
-#            # Now downsize if it doesn't fit
-#            if stdout.isatty() and not settings['ovirt-shell:wide'] and \
-#                    total > context.terminal.width:
-#                fraction = 1.0 * context.terminal.width / total
-#                fwidths = [0] * len(fields)
-#                # Pass 1: round down to nearest integer
-#                for i in range(len(fields)):
-#                    fwidths[i] = widths[i] * fraction
-#                    widths[i] = int(fwidths[i])
-#                # Pass 2: allocate fractional leftovers to columns
-#                available = context.terminal.width - 2 * len(fields) - sum(widths)
-#                remainders = [ (fwidths[i] - widths[i], i) for i in range(len(fields)) ]
-#                remainders.sort(reverse=True)
-#                for i in range(min(len(fields), available)):
-#                    widths[remainders[i][1]] += 1
-#            formats = ['%%-%d.%ds' % (w, w) for w in widths ]
-#            if settings['ovirt-shell:header']:
-#                # Header
-#                for i in range(len(fields)):
-#                    stdout.write(formats[i] % fields[i].name)
-#                    if i != len(fields) - 1:
-#                        stdout.write('  ')
-#                stdout.write('\n')
-#                # Horizontal line
-#                for i in range(len(fields)):
-#                    stdout.write('-' * widths[i])
-#                    if i != len(fields) - 1:
-#                        stdout.write('  ')
-#                stdout.write('\n')
-#            # Data elements
-##        for resource in getattr(collection, collection_member):
-#            for resource in collection:
-#                values = [ field.get(resource, self.context) for field in fields ]
-#                while sum([len(v) for v in values]) > 0:
-#                    for i in range(len(fields)):
-#                        stdout.write(formats[i] % values[i])
-#                        values[i] = values[i][widths[i]:]
-#                        if i != len(fields) - 1:
-#                            stdout.write('  ')
-#                    stdout.write('\n')
-#            stdout.write('\n')
 
     def format(self, context, result, show_all=False):
         self.context = context
