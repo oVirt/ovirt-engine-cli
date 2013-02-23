aa1d618b21ec2937bb0be49237f6707ccedcc4dc
Michael Pasternak <mpastern@redhat.com>
2013-02-23 12:32:52 +0200

    cli: make NO_SUCH_ACTION error a bit more clear
    Change-Id: If1c49aa0fd9e3a86477930c4f1c3816dac4ef903
    Signed-off-by: Michael Pasternak <mpastern@redhat.com>

--- src/cli/messages.py
+++ src/cli/messages.py
@@ -20,7 +20,7 @@ class Messages():
         OBJECT_IS_IMMUTABLE = '%s "%s" is immutable.'
         NO_SUCH_OPTION = 'option "%s" is not supported, see help for more details.'
         NO_SUCH_OBJECT = '%s "%s" does not exist.'
-        NO_SUCH_ACTION = 'action "%s" does not exist.'
+        NO_SUCH_ACTION = 'action "%s" does not exist in this context.'
         NO_SUCH_TYPE = 'type "%s" does not exist.'
         NO_SUCH_CONTEXT = 'cannot find any context for type "%s" using given arguments.'
         NO_SUCH_TYPE_OR_ARS_NOT_VALID = 'no such type "%s" or given arguments not valid.'
