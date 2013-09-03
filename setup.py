4987cec022bebae0b924da10a211e4279acc760b
Michael pasternak <mpastern@redhat.com>
2013-09-03 19:18:57 +0300

    cli: release 3.3.0.4-1
    move from SNAPSHOT to offical release
    
    Change-Id: I5a5cb0b071e3a8cf8174f41672b651ed26e8bd72
    Signed-off-by: Michael pasternak <mpastern@redhat.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-shell',
-    'version': '3.3.0.4-SNAPSHOT',
+    'version': '3.3.0.4-1',
     'description': 'A command-line interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
@@ -30,7 +30,7 @@ setup(
                  'ovirtcli.platform', 'ovirtcli.platform.posix',
                  'ovirtcli.platform.windows', 'ovirtcli.shell', 'ovirtcli.utils', 'cli',
                  'cli.command', 'cli.platform', 'cli.platform.posix'],
-    install_requires=[ 'ovirt-engine-sdk-python >= 3.3.0.3-SNAPSHOT', 'ply >= 3.3', 'kitchen >= 1' ],
+    install_requires=[ 'ovirt-engine-sdk-python >= 3.3.0.3', 'ply >= 3.3', 'kitchen >= 1' ],
     entry_points={ 'console_scripts': [ 'ovirt-shell = ovirtcli.main:main' ] },
     **version_info
 )
