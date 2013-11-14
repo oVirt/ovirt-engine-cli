3cb80da193dd8cd86720ec35c1cc86d3d82bdec9
Michael pasternak <mpastern@redhat.com>
2013-11-14 11:52:39 +0200

    cli: release 3.4.0.1-1
    Change-Id: I9fae2d02730f51ba46ab924cb626f86346a67b7a
    Signed-off-by: Michael pasternak <mpastern@redhat.com>

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-shell',
-    'version': '3.4.0.1-SNAPSHOT',
+    'version': '3.4.0.1-1',
     'description': 'A command-line interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
