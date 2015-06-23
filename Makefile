756bb8c528379a9c9999ac5ffb80ca6ceb3bea8c
Juan Hernandez <juan.hernandez@redhat.com>
2015-06-23 11:35:49 +0200

    cli: Prepare for next release
    In preparation for the next release this patch increases the version
    number and sets the RPM release number to 0.1.
    
    Change-Id: I745c400c3dd94857fa401a078c092b6bed6f2ea9
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 all: rpm
 
-rpmrelease:=1$(rpmsuffix)
-rpmversion=3.5.0.6
+rpmrelease:=0.1$(rpmsuffix)
+rpmversion=3.5.0.7
 RPMTOP=$(shell bash -c "pwd -P")/rpmtop
 SPEC=ovirt-engine-cli.spec
 
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-shell',
-    'version': '3.5.0.6',
+    'version': '3.5.0.7',
     'description': 'A command-line interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
