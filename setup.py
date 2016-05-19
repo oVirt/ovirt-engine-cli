942b2434a16e130579194bcddb9021234551cfa7
Juan Hernandez <juan.hernandez@redhat.com>
2016-05-19 12:31:49 +0200

    cli: Release 3.6.2.1
    Change-Id: I15e3bade3a3d5b332cbff9e8aee4b0e1e5a14aef
    Signed-off-by: Juan Hernandez <juan.hernandez@redhat.com>

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, Command
 
 version_info = {
     'name': 'ovirt-shell',
-    'version': '3.6.2.1preview0',
+    'version': '3.6.2.1',
     'description': 'A command-line interface to oVirt Virtualization',
     'author': 'Michael Pasternak',
     'author_email': 'mpastern@redhat.com',
